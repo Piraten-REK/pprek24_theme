f807fba344a2305cd6bf09e0234eac6be78a73dc
Mike Kühnapfel <16109180+veyxos@users.noreply.github.com>
2024-09-13 21:13:08 +0200

    switch from ts to js

--- create-release.js
+++ create-release.js
@@ -1,17 +1,15 @@
-import core from '@actions/core'
-import github from '@actions/github'
+const core = require('@actions/core')
+const github = require('@actions/github')
 
 async function run () {
     const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN')
     const TAG_NAME = core.getInput('version')
     const octokit = github.getOctokit(GITHUB_TOKEN)
 
-    octokit.rest.repos.createRelease
-
     const context = github.context ?? {}
     console.log({
-        owner: context.payload.repository!.owner!.login,
-        repo: context.payload.repository!.name,
+        owner: context.payload.repository?.owner?.login,
+        repo: context.payload.repository?.name,
         branch: context.ref,
         tag_name: TAG_NAME
     })
