7071f1e20f1d62c6a2794651141f07f78f294bef
Mike Kühnapfel <16109180+veyxos@users.noreply.github.com>
2024-09-13 21:18:18 +0200

    attempt to fix GITHUB_TOKEN not present in script

--- create-release.js
+++ create-release.js
@@ -2,17 +2,26 @@ const core = require('@actions/core')
 const github = require('@actions/github')
 
 async function run () {
-    const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN')
-    const TAG_NAME = core.getInput('version')
-    const octokit = github.getOctokit(GITHUB_TOKEN)
+    try {
+        const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN')
+        const TAG_NAME = core.getInput('version')
 
-    const context = github.context ?? {}
-    console.log({
-        owner: context.payload.repository?.owner?.login,
-        repo: context.payload.repository?.name,
-        branch: context.ref,
-        tag_name: TAG_NAME
-    })
+        if (!GITHUB_TOKEN) {
+            throw new Error('GITHUB_TOKEN is not set.')
+        }
+
+        // const octokit = github.getOctokit(GITHUB_TOKEN)
+
+        const context = github.context ?? {}
+        console.log({
+            owner: context.payload.repository?.owner?.login,
+            repo: context.payload.repository?.name,
+            branch: context.ref,
+            tag_name: TAG_NAME
+        })
+    } catch (error) {
+        core.setFailed(error.message)
+    }
 }
 
 void run()
