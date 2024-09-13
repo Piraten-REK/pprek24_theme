e98191d9e336aa6af647f20defa495b0d79517a8
Mike Kühnapfel <16109180+veyxos@users.noreply.github.com>
2024-09-13 21:03:50 +0200

    add final step to workflow

--- create-release.ts
+++ create-release.ts
@@ -3,6 +3,7 @@ import github from '@actions/github'
 
 async function run () {
     const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN')
+    const TAG_NAME = core.getInput('version')
     const octokit = github.getOctokit(GITHUB_TOKEN)
 
     octokit.rest.repos.createRelease
@@ -11,7 +12,8 @@ async function run () {
     console.log({
         owner: context.payload.repository!.owner!.login,
         repo: context.payload.repository!.name,
-        tag_name: context.ref,
+        branch: context.ref,
+        tag_name: TAG_NAME
     })
 }
 
