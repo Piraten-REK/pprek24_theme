6521d155de6966abb921e58938d1bd182adb5414
Mike Kühnapfel <16109180+veyxos@users.noreply.github.com>
2024-12-19 21:13:42 +0100

    add calendar page template

---
+++ pprek24/page-calendar.php
@@ -0,0 +1,20 @@
+<?php
+$type = 'unknown';
+if (isset($_GET['id'])) {
+  if (!empty($_GET['id']) && !empty($_GET['start'])) {
+    $type = 'event';
+  } else {
+    $type = 'broken_event';
+  }
+} elseif (isset($_GET['year']) && isset($_GET['month'])) {
+  $type = 'month';
+}
+
+get_header();
+
+if ($type == 'month'): ?>
+<h1>Unsere Termine</h1>
+<div class="spinner" data-pprek-calendar="month" data-pprek-year="<?php echo esc_attr($_GET['year']); ?>" data-pprek-month="<?php echo esc_attr($_GET['month']); ?>">PLACEHOLDER</div>
+<?php else: ?>
+KALENDER
+<?php endif; get_footer(); ?>
