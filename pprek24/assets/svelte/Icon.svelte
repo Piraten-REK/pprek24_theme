24d5170bc4bc70eaf0248c56098ee6128eb63548
Mike Kühnapfel <16109180+veyxos@users.noreply.github.com>
2024-12-22 21:33:58 +0100

    add Icon.svelte component

---
+++ pprek24/assets/svelte/Icon.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  interface Props {
+      icon: string
+      class?: string
+      [key: string]: unknown
+  }
+
+  const { icon, class: additionalClassName, ...rest }: Props = $props()
+  const classes = ['bi']
+  classes.push(`bi-${icon.replace(/([A-Z])/g, '-$1').toLowerCase()}`)
+  if (additionalClassName != null) {
+      classes.push(additionalClassName)
+  }
+  const className = $state(classes.join(' '))
+</script>
+
+<i
+  class={className}
+  role="img"
+  {...rest}
+></i>
