4a35e760ecf79bcfd247f6a4e8ff8271816341cd
Mike Kühnapfel <16109180+veyxos@users.noreply.github.com>
2025-01-05 22:39:59 +0100

    remove build files from repo

--- pprek24/assets/js/gutenberg/post-thumbnail.tsx.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-components', 'wp-compose', 'wp-data', 'wp-editor', 'wp-element', 'wp-i18n'), 'version' => '023964461fe47d167f19');
