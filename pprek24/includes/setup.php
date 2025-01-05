3560ede7f325b00f6299d9e5787f0b4181e1acb6
Mike Kühnapfel <16109180+veyxos@users.noreply.github.com>
2025-01-05 22:30:42 +0100

    add focus_point to post meta

--- pprek24/includes/setup.php
+++ pprek24/includes/setup.php
@@ -3,6 +3,25 @@
 function pprek24_init (): void {
   add_rewrite_rule('^site.webmanifest/?', 'index.php?webmanifest=1', 'top');
   add_rewrite_rule('^browserconfig.xml/?', 'index.php?browserconfig=1', 'top');
+
+  register_post_meta('post', 'pprek24_featured_image_focus_point', [
+    'type'          =>  'array',
+    'label'         =>  'Featured Image Focus',
+    'single'        =>  true,
+    'default'       =>  [50.0, 50.0],
+    'show_in_rest'  =>  [
+      'schema'  =>  [
+        'type'      =>  'array',
+        'items'     =>  [
+          'type'  =>  'number',
+          'min'   =>  0,
+          'max'   =>  100
+        ],
+        'minItems'  =>  2,
+        'maxItems'  =>  2
+      ]
+    ]
+  ]);
 }
 
 function pprek24_setup_theme (): void {
